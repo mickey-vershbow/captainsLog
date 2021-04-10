ebddc6567fb1ccf2147d25861083326b3987f713
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-04-09 23:05:07 -0400

    Index button hover styling

--- public/app.js
+++ public/app.js
@@ -1,8 +1,20 @@
 // mobile menu (nav bar hamburger menu)
 const burgerIcon = document.querySelector("#burger");
 const navbarMenu = document.querySelector(".navbar-menu");
+// Create A New Log Button on Index Page
+const newLogButton = document.querySelector("#create-button");
 
 burgerIcon.addEventListener("click", () => {
   console.log(burgerIcon);
   navbarMenu.classList.toggle("is-active");
+});
+
+newLogButton.addEventListener("mouseover", () => {
+  console.log(newLogButton);
+  newLogButton.classList.toggle("is-warning");
+});
+
+newLogButton.addEventListener("mouseout", () => {
+  console.log(newLogButton);
+  newLogButton.classList.toggle("is-warning");
 });
